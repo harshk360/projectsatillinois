467151f86a4d4ccc90072ccdc679fa4904b965b0
Sujay Khandekar <sskhandek@gmail.com>
2015-10-17 01:08:45 -0500

    Initial homepage changes

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -6,22 +6,23 @@ angular
     ])
     .config(function ($routeProvider) {
         $routeProvider
-            .when('/home/:pageId', {
-                templateUrl: 'static/views/home.html',
-                controller: 'HomeCtrl'
+            .when('/completed', {
+                templateUrl: 'static/views/completed.html',
+                controller:'HomeCtrl'
+            })
+            when('/inprogress', {
+                templateUrl: 'static/views/inprogress.html',
+                controller: 'InProgressCtrl'
             })
             .when('/profile/:profileId', {
                 templateUrl: 'static/views/profile.html',
                 controller: 'ProfileCtrl'
             })
-            .when('/event/:eventId', {
-                templateUrl: 'static/views/event.html',
-                controller: 'EventCtrl'
+            .when('/project/:projectId', {
+                templateUrl: 'static/views/project.html',
+                controller: 'CompletedCtrl'
             })
-            .otherwise({
-                templateUrl: 'static/views/home.html',
-                controller: 'HomeCtrl'
-            });
+            .otherwise('/completed');
     });
 
 angular.module('projects')
@@ -70,74 +71,12 @@ angular.module('projects')
                 })
         };
     })
-    .controller('HomeCtrl', function ($scope, $http, $routeParams) {
-        if ($routeParams.pageId === undefined){
-            $routeParams.pageId = 1;
-        }
-        $http
-            .get('/api/projects/page/' + $routeParams.pageId)
-            .success(function(res){
-                $scope.projects = res.projects;
-                $scope.number_of_pages = new Array(res.number_of_pages);
-                $scope.current_page = parseInt($routeParams.pageId);
-                //TODO: More Error Handling
-            }
-        );
+    .controller('CompletedCtrl', function ($scope, $http, $routeParams) {
 
     })
-    .controller('ProfileCtrl', function ($scope, $http, $routeParams) {
-        $http
-            .get('/api/user/' + $routeParams.profileId)
-            .success(function(routeUser){
-                $http
-                    .get('/api/user/current')
-                    .success(function(currUser){
-                        if (currUser.id === routeUser.id){
-                            $scope.setEditable = true;
-                            $scope.user = currUser;
-                        }else{
-                            $scope.setEditable = false;
-                            $scope.user = routeUser;
-                        }
-                    });
-            });
+    .controller('InProgressCtrl', function ($scope, $http, $routeParams) {
 
-        $scope.updateInfomation = function(){
-            var obj = {academic_major:$scope.user.academic_major, graduation_year: $scope.user.graduation_year}
-            $http
-                .post('/api/user/current/update',obj)
-                .success(function(){
-                    alert("Successfully updated!")
-                })
-        }
     })
-    .controller('EventCtrl', function ($scope, $routeParams, $http, $route) {
+    .controller('ProjectCtrl', function ($scope, $routeParams, $http, $route) {
 
-        $http
-            .get('/api/project/' + $routeParams.eventId)
-            .success(function(res) {
-                $scope.event = res;
-            
-            });
-        $http
-            .get('api/user/current')
-            .success(function(value){
-                 if (value.error){
-                     $scope.loggedIn = false;
-                 }else{
-                     $scope.user = value;
-                     $scope.loggedIn = true;
-                     $scope.user_id = value.id;
-                 }
-            });
-        $scope.changeStatus = function(status){
-            $http
-                .get('/api/event/' + $routeParams.eventId + '/signup/' + $scope.user_id + "/" + status)
-                .success(function (res){
-                    $scope.status = res.status;
-                    console.log(res);
-                    $route.reload();
-
-            });
-        };
     });

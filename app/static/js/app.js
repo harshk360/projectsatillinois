67fb45b6f55f1d2e56857c6cd80b060dcc651654
Sujay Khandekar <sskhandek@gmail.com>
2015-10-18 02:29:41 -0400

    Basic formatting for pages

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -74,14 +74,78 @@ angular.module('projects')
 
     })
     .controller('CompletedCtrl', function ($scope, $http, $routeParams, $location) {
+        $scope.projects = [
+            {
+                name: "Berwin For President",
+                description: "Berwin 2016.",
+                picture: "https://scontent-ord1-1.xx.fbcdn.net/hphotos-xat1/v/t1.0-9/11350732_10207699662912723_1646685888682476380_n.jpg?oh=ad89e16c1a884d1aaac7f4a403b19a50&oe=5689DB7F"
+            },
+            {
+                name: "Berwin For President",
+                description: "Berwin 2016.",
+                picture: "https://scontent-ord1-1.xx.fbcdn.net/hphotos-xat1/v/t1.0-9/11350732_10207699662912723_1646685888682476380_n.jpg?oh=ad89e16c1a884d1aaac7f4a403b19a50&oe=5689DB7F"
+            },
+            {
+                name: "Berwin For President",
+                description: "Berwin 2016.",
+                picture: "https://scontent-ord1-1.xx.fbcdn.net/hphotos-xat1/v/t1.0-9/11350732_10207699662912723_1646685888682476380_n.jpg?oh=ad89e16c1a884d1aaac7f4a403b19a50&oe=5689DB7F"
+            },
+            {
+                name: "Berwin For President",
+                description: "Berwin 2016.",
+                picture: "https://scontent-ord1-1.xx.fbcdn.net/hphotos-xat1/v/t1.0-9/11350732_10207699662912723_1646685888682476380_n.jpg?oh=ad89e16c1a884d1aaac7f4a403b19a50&oe=5689DB7F"
+            },
+            {
+                name: "Berwin For President",
+                description: "Berwin 2016.",
+                picture: "https://scontent-ord1-1.xx.fbcdn.net/hphotos-xat1/v/t1.0-9/11350732_10207699662912723_1646685888682476380_n.jpg?oh=ad89e16c1a884d1aaac7f4a403b19a50&oe=5689DB7F"
+            },
+            {
+                name: "Berwin For President",
+                description: "Berwin 2016.",
+                picture: "https://scontent-ord1-1.xx.fbcdn.net/hphotos-xat1/v/t1.0-9/11350732_10207699662912723_1646685888682476380_n.jpg?oh=ad89e16c1a884d1aaac7f4a403b19a50&oe=5689DB7F"
+            },
+
+        ];
+
         $scope.go = function ( path ) {
             $location.path( path );
         };
     })
     .controller('InProgressCtrl', function ($scope, $http, $routeParams, $location) {
+
+        $scope.projects = [
+            {
+                name: "Donald For President",
+                description: "Vote for Berwin Instead 2016.",
+                picture: "http://static6.businessinsider.com/image/55918b77ecad04a3465a0a63/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners.jpg"
+            },
+            {
+                name: "Donald For President",
+                description: "Vote for Berwin Instead 2016.",
+                picture: "http://static6.businessinsider.com/image/55918b77ecad04a3465a0a63/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners.jpg"
+            },
+            {
+                name: "Donald For President",
+                description: "Vote for Berwin Instead 2016.",
+                picture: "http://static6.businessinsider.com/image/55918b77ecad04a3465a0a63/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners.jpg"
+            },
+            {
+                name: "Donald For President",
+                description: "Vote for Berwin Instead 2016.",
+                picture: "http://static6.businessinsider.com/image/55918b77ecad04a3465a0a63/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners.jpg"
+            },
+            {
+                name: "Donald For President",
+                description: "Vote for Berwin Instead 2016.",
+                picture: "http://static6.businessinsider.com/image/55918b77ecad04a3465a0a63/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners.jpg"
+            },
+        ];
+
         $scope.go = function ( path ) {
             $location.path( path );
         };
+
     })
     .controller('ProjectCtrl', function ($scope, $routeParams, $http, $route) {
 
